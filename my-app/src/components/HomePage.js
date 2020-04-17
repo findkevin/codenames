ab8eb671a0c5346a7463cbd82fb3eb2068dd961b
Kevin Lam <find.keviin@gmail.com>
2020-04-17 15:28:14 -0400

    Experimenting with NPM History Package.

--- my-app/src/components/HomePage.js
+++ my-app/src/components/HomePage.js
@@ -1,22 +1,34 @@
 import React, { Component } from "react";
+import history from "../history/index";
 
 class HomePage extends Component {
-  constructor(props){
-    super(props);
-  }
+  // constructor(props) {
+  //   super(props);
+  // }
 
   render() {
     return (
       <div>
         <p>To join a game, enter the lobby name and click 'Enter.'</p>
-        <input type="text" id="game-name"/>
-        <button>Enter</button>
+        <input type="text" id="game-name" onKeyPress={this.handleKeyPress} />
+        <button onClick={this.startGame}>Enter</button>
       </div>
     );
   }
 
   //Helper Functions
+  //Create a function to get the string value of the game name from the URI.
+  startGame = () => {
+    const gameName = document.getElementById("game-name").value;
+    history.push("/" + gameName);
+  }
 
+  //Create a fn to listen for an event. If the event is ENTER, start the game.
+  handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      this.startGame();
+    }
+  };
 }
 
 export default HomePage;
