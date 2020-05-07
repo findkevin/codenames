94a0887f986dc233bf42bf112d559d0d4555b174
Kevin Lam <find.keviin@gmail.com>
2020-05-07 14:36:04 -0400

    Added serve script and npm installed serve

---
+++ build/precache-manifest.fecf9797d439467670491a4ca1c1a29c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7af6397e5fa7fbf461b56e55809e0844",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73697728247ad8541b61",
+    "url": "/static/css/main.53fc0339.chunk.css"
+  },
+  {
+    "revision": "bfb7f37781da75a0aff4",
+    "url": "/static/js/2.3a7e28a0.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/static/js/2.3a7e28a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73697728247ad8541b61",
+    "url": "/static/js/main.422d2ba3.chunk.js"
+  },
+  {
+    "revision": "0f7e17235ce2106ccee1",
+    "url": "/static/js/runtime-main.549be430.js"
+  },
+  {
+    "revision": "026246cf1a20e1a2f40fe09471cabac6",
+    "url": "/static/media/BlackCard.026246cf.png"
+  },
+  {
+    "revision": "0a6d886633578e2935749798e5a7310f",
+    "url": "/static/media/BlueCard.0a6d8866.png"
+  },
+  {
+    "revision": "506d15bc8e6b15db2ae7122cfb39a301",
+    "url": "/static/media/CodeNames.506d15bc.png"
+  },
+  {
+    "revision": "e87f5aba6f8316624a71a2c3f4ebfa05",
+    "url": "/static/media/NeutralCard.e87f5aba.png"
+  },
+  {
+    "revision": "e77b9cd473ac11eb5ac1a33039e481c5",
+    "url": "/static/media/RedCard.e77b9cd4.png"
+  }
+]);
