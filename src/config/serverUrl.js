c10cb6e2f6d5390deb49e3642fb7ef65c5b951de
Kevin Lam <find.keviin@gmail.com>
2020-05-09 16:42:56 -0400

    Changed server url to heroku server.

--- src/config/serverUrl.js
+++ src/config/serverUrl.js
@@ -1,4 +1,6 @@
 
-export const socketUrl = 'http://localhost:5000'//'https://kodonemu-server-pg.herokuapp.com/';
+export const socketUrl = "https://kodonemu-server-pg.herokuapp.com/";
+//'http://localhost:5000'
 
-export const serverUrl = 'http://localhost:5000/api/games' //"https://kodonemu-server-pg.herokuapp.com/api/games";
+export const serverUrl = "https://kodonemu-server-pg.herokuapp.com/api/games";
+//'http://localhost:5000/api/games'
